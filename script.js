09101be8722205a4b464da6f6d41798421d6b412
Skullkidd12 <araujo.skd@gmail.com>
2022-06-06 22:05:31 -0300

    um monte de coiso

---
+++ script.js
@@ -0,0 +1,12 @@
+let cadastrNome;
+let cadastroUsuario;
+let cadastroTextoCompleto;
+
+cadastroNome = window.prompt('qual e o seu nominho?')
+cadastroUsuario = "moises";
+cadastroTextoCompleto = "nome do usuário: ";
+cadastroTextoCompleto += cadastroNome;
+cadastroTextoCompleto += ", login do Usuário:";
+cadastroTextoCompleto += cadastroUsuario;
+
+console.log(cadastroTextoCompleto)
